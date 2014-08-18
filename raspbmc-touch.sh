09241523b19f20d479762e7f7fd7fe8c5f5ad507
Schlump <schlupp@vmail.me>
2014-08-18 20:43:11 +0200

    Update raspbmc-touch.sh

--- raspbmc-touch.sh
+++ raspbmc-touch.sh
@@ -4,8 +4,7 @@ then
   echo "Based on XBian touch installer by brantje edited by Schlump for raspbmc"
 
   echo "Generating udev rule"
-  echo "SUBSYSTEM==\"input\", ATTRS{name}==\"*eGalax Inc. USB TouchController*\",  ENV{DEVNAME}==\"*event*\", SYMLINK+=\"input/touchscreen\""
-  >> 95egalax.rules
+  echo "SUBSYSTEM==\"input\", ATTRS{name}==\"*eGalax Inc. USB TouchController*\",  ENV{DEVNAME}==\"*event*\", SYMLINK+=\"input/touchscreen\"" >> 95egalax.rules
 
   echo "Moving file..."
   sudo mv 95egalax.rules /etc/udev/rules.d/
