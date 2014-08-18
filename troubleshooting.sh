9015997a9843bf13737cd5fb19fb36e1afde767f
Schlump <schlupp@vmail.me>
2014-08-18 14:44:56 +0200

    Update troubleshooting.sh

--- troubleshooting.sh
+++ troubleshooting.sh
@@ -3,8 +3,12 @@ echo "toubleshooting touchscreen calibration"
 if [ -f /etc/pointercal ]; then
 echo "downloading & installing evtest"
 sudo apt-get install evtest
-echo "running  evtest, if uimapper is running there should be an entry called ""uimapper - touchscreen"
+echo "running  evtest"
+
+echo "if uimapper is running there should be an entry called ""uimapper - touchscreen"
+
 evtest
+
 echo "if theres no uimapper present -> uimapper is not starting right"
 echo "if theres no touchscreen present check your usb cable, maybe switch ports"
 
